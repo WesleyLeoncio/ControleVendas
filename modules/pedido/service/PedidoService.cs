520cb4fed6e2f3c57dff985c0e9b9f6a14cc4d1b
Your Name <wesley.leoncio.izi@gmail.com>
2025-02-11 19:11:21 -0300

    refactor: Atualizando class PedidoService.cs

--- modules/pedido/service/PedidoService.cs
+++ modules/pedido/service/PedidoService.cs
@@ -1,9 +1,12 @@
 ﻿using AutoMapper;
+using controle_vendas.infra.exceptions.custom;
 using controle_vendas.modules.common.unit_of_work.interfaces;
+using controle_vendas.modules.item_pedido.models.entity;
+using controle_vendas.modules.item_pedido.models.request;
 using controle_vendas.modules.pedido.models.entity;
 using controle_vendas.modules.pedido.models.request;
 using controle_vendas.modules.pedido.service.interfaces;
-
+using controle_vendas.modules.produto.models.entity;
 
 namespace controle_vendas.modules.pedido.service;
 
@@ -18,9 +21,51 @@ public class PedidoService : IPedidoService
         _mapper = mapper;
     }
 
-    public async Task<Pedido> CreatePedido(PedidoRequest request)
+
+    public async Task RegistrarPedido(PedidoRequest pedidoRequest)
     {
-        Pedido pedido = _mapper.Map<Pedido>(request);
-        return pedido;
+        //if (user == null) throw new NotFoundException("Vendedor não encontrado!");
+        Pedido pedido =  _mapper.Map<Pedido>(pedidoRequest);
+        pedido.VendedorId = "cadd2ea3-30bb-44a1-b409-ec65001fa6da";
+        foreach (var item in pedidoRequest.Itens)
+        {
+            await ValidarProduto(item);
+            
+            ItemPedido itemPedido = new ItemPedido
+            {
+                ProdutoId = item.ProdutoId,
+                Quantidade = item.Quantidade,
+            };
+           pedido.Itens.Add(itemPedido);
+        }
+        
+        Pedido entity = _uof.PedidoRepository.Create(pedido);
+        await _uof.Commit();
+        Console.WriteLine(entity);
+    }
+    
+    private async Task<Produto> CheckProduto(int id)
+    {
+        return await _uof.ProdutoRepository.GetAsync(p => p.Id == id) ??
+               throw new NotFoundException("Produto não encontrado!");
+    }
+
+    private async Task ValidarProduto(ItemPedidoRequest item)
+    {
+        Produto produto = await CheckProduto(item.ProdutoId);
+        await RetirarQuantidadeProduto(produto, item.Quantidade);
+        
+    }
+
+    private async Task RetirarQuantidadeProduto(Produto produto, int quantidade)
+    {
+        if (produto.Estoque < quantidade)
+        {
+            throw new NotFoundException($"Não a produtos o suficiente em estoque, " 
+                                        + $"Quandidade em estoque: {produto.Estoque}");
+        }
+        produto.Estoque = produto.Estoque - quantidade;
+        _uof.ProdutoRepository.Update(produto);
+        await _uof.Commit();
     }
 }
