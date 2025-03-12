0aec86762d7ba01e6881114087f43a3158c18f08
Your Name <wesley.leoncio.izi@gmail.com>
2025-03-12 20:57:10 -0300

    feat: Adição do pacote FluentAssert no projeto.

--- ControleVendasTeste/UnitTest1.cs
+++ ControleVendasTeste/UnitTest1.cs
@@ -1,9 +1,12 @@
-﻿namespace ControleVendasTeste;
+﻿using FluentAssertions;
+
+namespace ControleVendasTeste;
 
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
+        
     }
 }
