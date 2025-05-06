876c5befd8117235820dddc399d66b890d9cac7c
Your Name <wesley.leoncio.izi@gmail.com>
2025-05-06 13:52:47 -0300

    feat: Atualizando os arquivos do filtro de cliente

---
+++ ControleVendas/Modules/Cliente/Repository/Filter/Custom/FilterClienteAtivo.cs
@@ -0,0 +1,19 @@
+﻿using ControleVendas.Modules.Cliente.Models.Entity;
+using ControleVendas.Modules.Cliente.Models.Request;
+using ControleVendas.Modules.Cliente.Repository.Filter.Interfaces;
+
+namespace ControleVendas.Modules.Cliente.Repository.Filter.Custom;
+
+public class FilterClienteAtivo : IFilterClienteResult
+{
+    public IQueryable<ClienteEntity> RunFilter(IQueryable<ClienteEntity> queryable, ClienteFiltroRequest filtro)
+    {
+        if (filtro.Ativo != null)
+        {
+            queryable = queryable.Where(q => q.Ativo == filtro.Ativo);
+            return queryable;
+        }
+
+        return queryable;
+    }
+}
---
+++ ControleVendas/Modules/Cliente/Repository/Filter/Interfaces/IFilterClienteResult.cs
@@ -0,0 +1,9 @@
+﻿using ControleVendas.Modules.Cliente.Models.Entity;
+using ControleVendas.Modules.Cliente.Models.Request;
+
+namespace ControleVendas.Modules.Cliente.Repository.Filter.Interfaces;
+
+public interface IFilterClienteResult
+{
+    public IQueryable<ClienteEntity> RunFilter(IQueryable<ClienteEntity> queryable, ClienteFiltroRequest filtro);
+}
